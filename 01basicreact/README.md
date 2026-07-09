c81a7ae9c1ab307485cb693c940f5b7da3b83afc
Vansh Nanda <vanshnanda2006@gmail.com>
2026-07-09 23:39:50 +0530

    Remove 01basicreact submodule and recreate as proper directory

---
+++ 01basicreact/README.md
@@ -0,0 +1,3 @@
+# 01basicreact
+
+A simple React project directory recreated as a normal folder, not a submodule.
---
+++ 01basicreact/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>01basicreact</h1>
+      <p>This is a normal React app directory, no submodule.</p>
+    </div>
+  );
+}
+
+export default App;
