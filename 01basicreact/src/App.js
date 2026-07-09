0db9df3d14cc262698d989e87838f71fc812b77f
Vansh Nanda <vanshnanda2006@gmail.com>
2026-07-09 23:44:59 +0530

    Restore chai.js component and complete 01basicreact app structure

--- 01basicreact/src/App.js
+++ 01basicreact/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Chai from './chai';
 
 function App() {
   return (
     <div>
       <h1>01basicreact</h1>
-      <p>This is a normal React app directory, no submodule.</p>
+      <Chai />
     </div>
   );
 }
---
+++ 01basicreact/src/chai.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Chai() {
+  return (
+    <div>
+      <h3>chai is ready</h3>
+    </div>
+  );
+}
+
+export default Chai;
